b91fbd9a4f2f492f7c6476ad7bde05133f22ddb6
Russell Toris <rctoris@wpi.edu>
2013-04-15 10:14:45 -0700

    r2 build

--- build/keyboardteleop.js
+++ build/keyboardteleop.js
@@ -3,7 +3,7 @@
  */
 
 var KEYBOARDTELEOP = KEYBOARDTELEOP || {
-  REVISION : '2-devel'
+  REVISION : '2'
 };
 
 /**
@@ -14,7 +14,7 @@ var KEYBOARDTELEOP = KEYBOARDTELEOP || {
  * Manages connection to the server and all interactions with ROS.
  *
  * Emits the following events:
- *  * 'change' - emitted with a change in speed occurs
+ *   * 'change' - emitted with a change in speed occurs
  *
  * @constructor
  * @param options - possible keys include:
@@ -24,7 +24,7 @@ var KEYBOARDTELEOP = KEYBOARDTELEOP || {
  */
 KEYBOARDTELEOP.Teleop = function(options) {
   var that = this;
-  var options = options || {};
+  options = options || {};
   var ros = options.ros;
   var topic = options.topic || '/cmd_vel';
   // permanent throttle
@@ -64,7 +64,7 @@ KEYBOARDTELEOP.Teleop = function(options) {
         break;
       case 87:
         // up
-        x = .5 * speed;
+        x = 0.5 * speed;
 
         break;
       case 68:
@@ -73,15 +73,15 @@ KEYBOARDTELEOP.Teleop = function(options) {
         break;
       case 83:
         // down
-        x = -.5 * speed;
+        x = -0.5 * speed;
         break;
       case 69:
         // strafe right
-        y = -.5 * speed;
+        y = -0.5 * speed;
         break;
       case 81:
         // strafe left
-        y = .5 * speed;
+        y = 0.5 * speed;
         break;
     }
 
